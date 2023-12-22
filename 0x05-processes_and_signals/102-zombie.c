2b377e21e0277780fa4cd4bb6b663783f5e457d8
Badr_Annabi <badrannabi@gmail.com>
2023-12-22 23:22:22 +0100

    TASK 11

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+/**
+ * infinite_while - Create an infinite loop.
+ *
+ * Return: Always 0.
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+/**
+ * main - Displays zombie processes
+ * Return: Always 0
+ */
+int main(void)
+{
+	pid_t zombie_pid;
+	int i;
+
+	for (i = 0; i < 5; i++)
+	{
+		zombie_pid = fork();
+		if (zombie_pid == -1)
+			exit (1);
+		else if (zombie_pid == 0)
+		{
+			exit (0);
+		}
+		else
+		{
+			printf("Zombie process created, PID: %d\n", zombie_pid);
+		}
+	}
+	infinite_while();
+	return (0);
+}
