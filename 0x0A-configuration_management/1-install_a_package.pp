59cf358032b4a5f88978ba6a7b42c4dbbc67d7ba
Badr-annabi <badrannabi@gmail.com>
2024-02-23 22:02:51 +0000

    TASK 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,6 @@
-#This script installs flask
-package { 'flask':
-ensure          => 'present',
-provider        => 'pip3',
-install_options => ['==2.1.0'],
+#!/usr/bin/pup
+# Install a specific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
