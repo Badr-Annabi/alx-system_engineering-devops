24462f57ab0a50a58469276359f3a35299130f3a
Badr-annabi <badrannabi@gmail.com>
2024-02-25 14:38:49 +0000

    TASK 4

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,11 +1,17 @@
 #This script connect to a server without typing a password.
 
+file {'/etc/ssh/ssh_config':
+ensure => present,
+}
+
 file_line { 'Turn off password auth':
-path => '/etc/ssh/ssh_config',
-line => 'PasswordAuthentification no',
+path  => '/etc/ssh/ssh_config',
+line  => 'PasswordAuthentification no',
+match => '^#PasswordAuthentification',
 }
 
 file_line {'Declare identity file':
-path => '/etc/ssh/ssh_config',
-line => 'IdentityFile ~/.ssh/school',
+path  => '/etc/ssh/ssh_config',
+line  => 'IdentityFile ~/.ssh/school',
+match => '^#IdentityFile',
 }
