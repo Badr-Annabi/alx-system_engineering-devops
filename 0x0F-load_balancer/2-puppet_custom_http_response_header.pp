13724f6f304dfab5a381aa36744e51d7442032ac
Badr-annabi <badrannabi@gmail.com>
2024-03-05 00:49:37 +0000

    TASK 2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,20 @@
+#This puppet script automate the task of creating a custom HTTP header
+#response
+
+exec {'update':
+command => 'usr/bin/apt-get update',
+}
+
+package {'nginx':
+ensure => 'present',
+}
+
+file_line { 'http_header':
+path  => '/etc/nginx/nginx.conf',
+match => 'http {',
+line  => "http {\n\t\add_header X-Served-By \"${hostname}\";",
+}
+
+exec {'run':
+command => '/usr/sbin/service nginx restart',
+}
