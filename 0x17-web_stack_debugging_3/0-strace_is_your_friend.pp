33df94aa2738ace1e94cfa32733a02f22af5a735
Badr-annabi <badrannabi@gmail.com>
2024-04-11 17:28:18 +0000

    double quotes

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 #Fix the 500 internal error by changing phpp with php
 exec {'fix-wp':
-command => "sed -i s/phpp/php/g /var/www/html/wp-settings.php",
+command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
 path    => '/usr/local/bin/:/bin/'
 }
